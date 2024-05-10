595ce8e8606b13e8aaf1f00632d4717d9a61a75c
DragonCode <151834618+Whitedragon115@users.noreply.github.com>
2024-05-10 16:24:51 +0800

    zip progrem

---
+++ CreateZip.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const archiver = require('archiver');
+
+const nowdate = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').replace(/:/g, '-');
+const output = fs.createWriteStream(path.join(__dirname, `[ ZIP FILE ]/KKSMP-${nowdate}.zip`));
+const archive = archiver('zip', { zlib: { level: 5 } });
+
+output.on('close', () => { console.log(`Archive created. Total bytes: ${archive.pointer()}`) });
+archive.on('error', (err) => { throw err });
+archive.pipe(output);
+
+const archive_file = ['index.js', 'config.json', 'package.json', 'deploy.js', '.env'];
+const acchive_folder = ['commands', 'events', 'function', 'unity'];
+
+for (const file of archive_file) {
+    archive.file(path.join(__dirname, file), { name: file })
+}
+
+for (const folder of acchive_folder) {
+    archive.directory(path.join(__dirname, folder), folder)
+}
+
+archive.finalize();
+
+
+
